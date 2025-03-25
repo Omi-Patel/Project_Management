ed6691870f25f59d7b5441ad2e325fe1bbb17afd
Om Patel <omipatel7113@gmail.com>
2025-03-25 10:45:39 +0530

    initial setup

---
+++ src/test/kotlin/com/pms/pms/ProjectManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pms.pms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
