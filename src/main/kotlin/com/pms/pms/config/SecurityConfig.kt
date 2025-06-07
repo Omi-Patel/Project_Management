649079c1f78cc120943cdaf0ad3b164ab3650bb8
Om Patel <omipatel7113@gmail.com>
2025-06-07 15:44:56 +0530

    add frotend url to solve cors

--- src/main/kotlin/com/pms/pms/config/SecurityConfig.kt
+++ src/main/kotlin/com/pms/pms/config/SecurityConfig.kt
@@ -43,7 +43,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:5173")
+        configuration.allowedOrigins = listOf("http://localhost:5173", "https://project-management-client-eosin.vercel.app")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("Authorization", "Content-Type")
         configuration.allowCredentials = true
